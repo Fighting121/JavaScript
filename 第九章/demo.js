99f18e1f08b8719100e921e17aa5c7cf511fe0f5
CodeMrZB <18838197439@163.com>
2019-01-03 19:20:00 +0800

    提交代码

---
+++ 第九章/demo.js
@@ -0,0 +1,12 @@
+var box = new Date();
+alert(box);
+
+alert(Date.parse('6/13/2011'));
+
+alert(Date.parse());
+
+alert(new Date(Date.parse('6/13/2011')));
+
+alert(new Date('6/13/2011'));
+
+alert(Date.UTC(2007, 10, 15, 17, 22, 45, 15));
--- 第八章/demo.js
+++ 第八章/demo.js
@@ -1,1 +1,10 @@
 alert("对象和数组");
+var box = {
+    name: '张三',
+    age: 18,
+    run: function() {
+        alert("哈哈哈哈");
+    }
+}
+alert(box.name +',' + box.age);
+box.run();
